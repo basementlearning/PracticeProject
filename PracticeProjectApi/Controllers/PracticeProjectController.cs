0448af9141627afb7824de568504b6b779759b47
Josh Smith <jasmithspriority@gmail.com>
2019-06-06 22:15:41 -0400

    Adjustments

--- PracticeProjectApi/Controllers/PracticeProjectController.cs
+++ PracticeProjectApi/Controllers/PracticeProjectController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 namespace PracticeProjectApi.Controllers
 {
 	[Route("api/[controller]")]
-	public class PracticeProjectController : Controller
+	public class PracticeProjectController : ControllerBase
 	{
 
 
